feada055b8e39c6493eac29cd4852e420742a16a
Beto Martinez <bettomb@gmail.com>
2021-05-29 17:34:46 -0500

    Snake movement implemented

--- snake/models/snake.rb
+++ snake/models/snake.rb
@@ -1,9 +1,40 @@
 module Model
     class Snake < Struct.new(:coords)
+
+         
         def draw(state)
             coords.each do |coord|                
                 coord.draw(state)
-            end            
+            end                                              
+            move_snake()
+        end
+
+        
+        private
+        
+        def move
+            
+            new_c = get_next_coord()
+            
+            
+            new_coords = [new_c] + self.coords[0...-1]
+            self.coords = new_coords
+        end
+        
+        def get_next_coord
+            last_coord = self.coords[-1]
+            new_coord = [self.coords.first.x+([1, 0][0]), self.coords.first.y+([1, 0][1])]            
+            new_coord = Model::Coord.new(new_coord[0], new_coord[1])
+            last_coord.remove()
+            return new_coord
+        end
+        
+        def move_snake()
+            
+            
+            move()
+            sleep 0.5
+        
         end
     end    
 end
