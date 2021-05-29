f198e906d3c736a4bccf83ac0d3a487734b7af1a
Beto Martinez <bettomb@gmail.com>
2021-05-29 17:31:08 -0500

    One more Coord to prevent some kind of overflow on pop

--- snake/controller/state.rb
+++ snake/controller/state.rb
@@ -3,16 +3,17 @@ module State
     class Game < Struct.new(:grid, :food, :snake)
 
         def draw            
-            food.draw(self)
-            snake.draw(self)                        
+            food.draw(self) 
+            snake.draw(self)                                   
         end
+        
     end
     
     def self.get_state
         State::Game.new(
             Model::Grid.new(40, 30, 20),
             Model::Food.new(20, 20),
-            Model::Snake.new([Model::Coord.new(1, 0), Model::Coord.new(0, 0)])
+            Model::Snake.new([Model::Coord.new(2, 0), Model::Coord.new(1, 0), Model::Coord.new(0, 0)])
         )        
     end
 end
