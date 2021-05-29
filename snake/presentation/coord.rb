0fe10dc1438ccd0eb3d51ceb33dadabb311c0905
Beto Martinez <bettomb@gmail.com>
2021-05-29 17:33:34 -0500

    Initial size to 0 in the coord and remove method

--- snake/presentation/coord.rb
+++ snake/presentation/coord.rb
@@ -5,14 +5,19 @@ module Model
 
         def initialize(x, y)
             super(x, y)   
-            @fig = Square.new(x: @x, y: @y, color: 'red')
+            @fig = Square.new(x: @x, y: @y, color: 'red', size: 0)            
         end
 
         def draw(state)
             @fig.x = (x * state.grid.ratio)
             @fig.y = (y * state.grid.ratio)
-            @fig.size = state.grid.ratio            
+            @fig.size = state.grid.ratio
+            @fig.add()            
         end      
+
+        def remove
+            @fig.remove()
+        end
 
         def set_color(new_color)
             @fig.color = new_color
